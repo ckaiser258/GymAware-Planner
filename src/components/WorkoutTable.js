41e63d837d922929d94a81e0643c33264df2b8db
Colton <kaiserc2@seattleu.edu>
2020-11-05 15:59:20 -0800

    Make skeleton workout table.

--- src/components/WorkoutTable.js
+++ src/components/WorkoutTable.js
@@ -1,14 +1,35 @@
-import React from 'react'
-import {Table, TableContainer, TableBody, TableCell, TableHead, TableRow, Paper} from '@material-ui/core'
+import React from "react";
+import {
+  Table,
+  TableContainer,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+  Paper,
+  Container,
+} from "@material-ui/core";
 
 function WorkoutTable() {
-    return (
-        <TableContainer component={Paper}>
-            <Table>
-                
-            </Table>
-        </TableContainer>
-    )
+  return (
+    <Container>
+      <TableContainer component={Paper}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell style={{fontWeight: "bold"}}>Exercise</TableCell>
+              <TableCell style={{fontWeight: "bold"}}>Target Velocity (m/s)</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            <TableRow>
+              <TableCell>Hello</TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Container>
+  );
 }
 
-export default WorkoutTable
+export default WorkoutTable;
