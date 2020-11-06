a9462b5754b42a412d9b66f6aebec04a03506e16
Colton <kaiserc2@seattleu.edu>
2020-11-05 16:00:05 -0800

    Add select workout day option to App.js.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import './App.css';
-import Form from './components/Form'
-import WorkoutTable from './components/WorkoutTable';
+import React, { useState } from "react";
+import "./App.css";
+import Form from "./components/Form";
+import WorkoutTable from "./components/WorkoutTable";
+import { TextField, MenuItem } from "@material-ui/core";
 
 function App() {
+  const [day, setDay] = useState("Select Your Day")
   return (
     <div className="App">
+      <TextField select helperText="What day is it?" value={day}>
+        <MenuItem>Lower Body</MenuItem>
+        <MenuItem>Upper Body</MenuItem>
+      </TextField>
       <Form />
       <WorkoutTable />
     </div>
