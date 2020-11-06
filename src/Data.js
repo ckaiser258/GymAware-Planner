92779ea76f7a2ea6f76e621743641e998ca5197a
Colton <kaiserc2@seattleu.edu>
2020-11-06 13:45:45 -0800

    Create data file with exercises and velocities.

---
+++ src/Data.js
@@ -0,0 +1,41 @@
+const UpperBody = [
+  { name: "Bench Press" },
+  { name: "Overhead Press" },
+  { name: "Bent-Over BB Row" },
+  { name: "Pull-Ups" },
+  { name: "DB Bicep Curl" },
+  { name: "Banded Tricep Pushdown" },
+  { name: "Russian Twist" },
+];
+
+const LowerBody = [
+  { name: "Squat" },
+  { name: "RDL" },
+  { name: "Forward Lunge" },
+  { name: "Bulgarian SS" },
+  { name: "Nordic Hamstring Lowering" },
+  { name: "Stability Ball Plank" },
+];
+
+const Velocities = [
+  {
+    name: "Absolute Strength",
+    range: "0.15 - 0.35",
+  },
+  {
+    name: "Accelerative Strength",
+    range: "0.45 - 0.75",
+  },
+  {
+    name: "Strength-Speed",
+    range: "0.75 - 1.0",
+  },
+  {
+    name: "Speed-Strength",
+    range: "1.0 - 1.5",
+  },
+  {
+    name: "Starting Strength",
+    range: ">1.5",
+  },
+];
