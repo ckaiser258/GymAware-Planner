f0a4488dcae52cc190e755c3d683f20fb2928511
Colton <kaiserc2@seattleu.edu>
2020-11-07 13:24:13 -0800

    Add velocity name state to Form.

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,35 +1,49 @@
-import React, { useState } from "react";
-import WorkoutTable from "./WorkoutTable"
+import React, { useState, useEffect } from "react";
+import WorkoutTable from "./WorkoutTable";
 import { Data } from "../Data";
 import { TextField, MenuItem } from "@material-ui/core";
 
-function Form({day}) {
-  const [velocity, setVelocity] = useState({});
+function Form({ day }) {
+  const [velocityRange, setVelocityRange] = useState("");
+  const [velocityName, setVelocityName] = useState("");
 
   //Set velocity to menu value
-  const changeVelocity = (e) => setVelocity(e.target.value);
+  const changeVelocity = (e) => setVelocityRange(e.target.value);
+
+  const fetchVelocityName = () => {
+    const currentVelocity = Data.Velocities.filter((velocity) => {
+      return velocity.range === velocityRange;
+    });
+    if (currentVelocity[0]) {
+      setVelocityName(currentVelocity[0].name);
+    }
+  };
+
+  useEffect(() => {
+    fetchVelocityName()
+  }, [velocityRange])
 
   return (
-      <>
-    <div>
-      <TextField
-        select
-        onChange={changeVelocity}
-        value={velocity}
-        helperText="Please select your max velocity from last phase."
-      >
-        {Data.Velocities.map((velocity) => {
-          return (
-            <MenuItem onChange={changeVelocity} value={velocity}>
-              {velocity.range}
-            </MenuItem>
-          );
-        })}
-      </TextField>
-    </div>
-    <div>
-        <WorkoutTable day={day} velocity={velocity} />
-    </div>
+    <>
+      <div>
+        <TextField
+          select
+          onChange={changeVelocity}
+          value={velocityRange}
+          helperText="Please select your max velocity from last phase."
+        >
+          {Data.Velocities.map((velocity) => {
+            return (
+              <MenuItem onChange={changeVelocity} value={velocity.range}>
+                {velocity.range}
+              </MenuItem>
+            );
+          })}
+        </TextField>
+      </div>
+      <div>
+        <WorkoutTable day={day} velocityRange={velocityRange} velocityName={velocityName} />
+      </div>
     </>
   );
 }
